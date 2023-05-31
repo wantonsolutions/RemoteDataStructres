6cc6d5abdf19cd530e96a6dd57809d9caf85069c
wantonsolutions <stewbertgrant@gmail.com>
2023-05-30 18:58:36 -0700

    templated out state machines

---
+++ rcuckoo_rdma/cuckoo.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CUCKOO_H
+#define CUCKOO_H
+
+#include "state_machines.h"
+
+using namespace cuckoo_state_machines;
+
+namespace cuckoo_rcuckoo {
+    class RCuckoo : public Client_State_Machine {
+
+    }
+}
+
+#endif
