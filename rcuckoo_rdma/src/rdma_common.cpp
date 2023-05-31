fc2986c6f7b08a1c6c297e9e9218742d182dc00d
wantonsolutions <stewbertgrant@gmail.com>
2023-05-31 15:48:01 -0700

    dropped in rdma files in c, converting to valid c++

---
+++ rcuckoo_rdma/src/rdma_common.cpp
@@ -0,0 +1,299 @@
+/*
+ * Implementation of the common RDMA functions. 
+ *
+ * Authors: Animesh Trivedi
+ *          atrivedi@apache.org 
+ */
+
+#include "rdma_common.h"
+
+
+void show_rdma_cmid(struct rdma_cm_id *id)
+{
+	if(!id){
+		rdma_error("Passed ptr is NULL\n");
+		return;
+	}
+	printf("RDMA cm id at %p \n", (void * )id);
+	if(id->verbs && id->verbs->device)
+		printf("dev_ctx: %p (device name: %s) \n", (void *)id->verbs, 
+			id->verbs->device->name);
+	if(id->channel)
+		printf("cm event channel %p\n", (void *)id->channel);
+	printf("QP: %p, port_space %x, port_num %u \n", (void *)id->qp, 
+			id->ps,
+			id->port_num);
+}
+
+void show_rdma_buffer_attr(struct rdma_buffer_attr *attr){
+	if(!attr){
+		rdma_error("Passed attr is NULL\n");
+		return;
+	}
+	printf("---------------------------------------------------------\n");
+	printf("buffer attr, addr: %p , len: %u , stag : 0x%x \n", 
+			(void*) attr->address, 
+			(unsigned int) attr->length,
+			attr->stag.local_stag);
+	printf("---------------------------------------------------------\n");
+}
+
+struct ibv_mr* rdma_buffer_alloc(struct ibv_pd *pd, uint32_t size,
+    enum ibv_access_flags permission) 
+{
+	struct ibv_mr *mr = NULL;
+	if (!pd) {
+		rdma_error("Protection domain is NULL \n");
+		return NULL;
+	}
+	void *buf = calloc(1, size);
+	if (!buf) {
+		rdma_error("failed to allocate buffer, -ENOMEM\n");
+		return NULL;
+	}
+	debug("Buffer allocated: %p , len: %u \n", buf, size);
+	mr = rdma_buffer_register(pd, buf, size, permission);
+	if(!mr){
+		free(buf);
+	}
+	return mr;
+}
+
+
+struct ibv_mr *rdma_buffer_register(struct ibv_pd *pd, 
+		void *addr, uint32_t length, 
+		enum ibv_access_flags permission)
+{
+	struct ibv_mr *mr = NULL;
+	if (!pd) {
+		rdma_error("Protection domain is NULL, ignoring \n");
+		return NULL;
+	}
+	mr = ibv_reg_mr(pd, addr, length, permission);
+	if (!mr) {
+		rdma_error("Failed to create mr on buffer, errno: %d \n", -errno);
+		return NULL;
+	}
+	debug("Registered: %p , len: %u , stag: 0x%x \n", 
+			mr->addr, 
+			(unsigned int) mr->length, 
+			mr->lkey);
+	return mr;
+}
+
+void print_dev_attributes(struct ibv_device_attr_ex * attr) {
+	printf("Fetch ADD %X\n",attr->pci_atomic_caps.fetch_add);
+	printf("SWAP %X\n",attr->pci_atomic_caps.swap);
+	printf("CAS %X\n",attr->pci_atomic_caps.compare_swap);
+}
+
+struct ibv_mr* rdma_buffer_alloc_dm(struct ibv_pd *pd, uint32_t size, enum ibv_access_flags permission) {
+	
+	struct ibv_alloc_dm_attr dm_attr = {0};
+	struct ibv_dm             *dm = {0};
+	struct ibv_mr			  *mr ={0};
+
+	struct ibv_device_attr_ex attrx;
+
+	if (ibv_query_device_ex(pd->context, NULL, &attrx)) {
+		printf("unable to query device for device memory alloc\n");
+		return NULL;
+	}
+
+	printf("max alloc size %d\n",attrx.max_dm_size);
+	if (!attrx.max_dm_size) {
+		printf("Device doesn't support dm allocation\n");
+		return NULL;
+	}
+
+	if (attrx.max_dm_size < size) {
+		rdma_error("Size of %ld larger than max alloc %ll",size,attrx.max_dm_size);
+		return NULL;
+	}
+
+	if (attrx.max_dm_size != DEVICE_MEMORY_KB) {
+		printf("Defined value for max device memory not the same real: %ld static %d, are you running on CX5?\n",attrx.max_dm_size,DEVICE_MEMORY_KB);
+	}
+
+	print_dev_attributes(&attrx);
+
+	dm_attr.length = size;
+	dm = ibv_alloc_dm(pd->context, &dm_attr);
+
+
+	if (!dm) {
+		printf("Unable to allocate device memory\n");
+		return NULL;
+	}
+
+	// a bit of a c++ switcheroo for bullshit reasons
+	unsigned int p_sub = (unsigned int)permission;
+	p_sub = p_sub | IBV_ACCESS_ZERO_BASED;
+	permission = (enum ibv_access_flags)p_sub;
+
+	mr = ibv_reg_dm_mr(pd,dm,0,size,permission);
+
+	if (!mr) {
+		//rdma_error("Unable to register device memory\n");
+		printf("Unable to register device memory\n");
+		return NULL;
+	}
+
+	return mr;
+
+
+}
+
+struct ibv_mr *rdma_buffer_register_dm(struct ibv_pd *pd, 
+		void *addr, uint32_t length, 
+		enum ibv_access_flags permission)
+{
+
+}
+
+void rdma_buffer_free(struct ibv_mr *mr) 
+{
+	if (!mr) {
+		rdma_error("Passed memory region is NULL, ignoring\n");
+		return ;
+	}
+	void *to_free = mr->addr;
+	rdma_buffer_deregister(mr);
+	debug("Buffer %p free'ed\n", to_free);
+	free(to_free);
+}
+
+void rdma_buffer_deregister(struct ibv_mr *mr) 
+{
+	if (!mr) { 
+		rdma_error("Passed memory region is NULL, ignoring\n");
+		return;
+	}
+	debug("Deregistered: %p , len: %u , stag : 0x%x \n", 
+			mr->addr, 
+			(unsigned int) mr->length, 
+			mr->lkey);
+	ibv_dereg_mr(mr);
+}
+
+int process_rdma_cm_event(struct rdma_event_channel *echannel, 
+		enum rdma_cm_event_type expected_event,
+		struct rdma_cm_event **cm_event)
+{
+	int ret = 1;
+	ret = rdma_get_cm_event(echannel, cm_event);
+	if (ret) {
+		rdma_error("Failed to retrieve a cm event, errno: %d \n",
+				-errno);
+		return -errno;
+	}
+	/* lets see, if it was a good event */
+	if(0 != (*cm_event)->status){
+		rdma_error("CM event has non zero status: %d\n", (*cm_event)->status);
+		ret = -((*cm_event)->status);
+		/* important, we acknowledge the event */
+		rdma_ack_cm_event(*cm_event);
+		return ret;
+	}
+	/* if it was a good event, was it of the expected type */
+	if ((*cm_event)->event != expected_event) {
+		rdma_error("Unexpected event received: %s [ expecting: %s ]", 
+				rdma_event_str((*cm_event)->event),
+				rdma_event_str(expected_event));
+		/* important, we acknowledge the event */
+		rdma_ack_cm_event(*cm_event);
+		return -1; // unexpected event :(
+	}
+	debug("A new %s type event is received \n", rdma_event_str((*cm_event)->event));
+	/* The caller must acknowledge the event */
+	return ret;
+}
+
+
+int process_work_completion_events (struct ibv_comp_channel *comp_channel, 
+		struct ibv_wc *wc, int max_wc)
+{
+	struct ibv_cq *cq_ptr = NULL;
+	void *context = NULL;
+	int ret = -1, i, total_wc = 0;
+       /* We wait for the notification on the CQ channel */
+	ret = ibv_get_cq_event(comp_channel, /* IO channel where we are expecting the notification */ 
+		       &cq_ptr, /* which CQ has an activity. This should be the same as CQ we created before */ 
+		       &context); /* Associated CQ user context, which we did set */
+       if (ret) {
+	       rdma_error("Failed to get next CQ event due to %d \n", -errno);
+	       return -errno;
+       }
+       /* Request for more notifications. */
+       ret = ibv_req_notify_cq(cq_ptr, 0);
+       if (ret){
+	       rdma_error("Failed to request further notifications %d \n", -errno);
+	       return -errno;
+       }
+       /* We got notification. We reap the work completion (WC) element. It is 
+	* unlikely but a good practice it write the CQ polling code that 
+       * can handle zero WCs. ibv_poll_cq can return zero. Same logic as 
+       * MUTEX conditional variables in pthread programming.
+	*/
+       total_wc = 0;
+       do {
+	       ret = ibv_poll_cq(cq_ptr /* the CQ, we got notification for */, 
+		       max_wc - total_wc /* number of remaining WC elements*/,
+		       wc + total_wc/* where to store */);
+	       if (ret < 0) {
+		       rdma_error("Failed to poll cq for wc due to %d \n", ret);
+		       /* ret is errno here */
+		       return ret;
+	       }
+	       total_wc += ret;
+       } while (total_wc < max_wc); 
+       debug("%d WC are completed \n", total_wc);
+
+       /* Now we check validity and status of I/O work completions */
+       for( i = 0 ; i < total_wc ; i++) {
+	       if (wc[i].status != IBV_WC_SUCCESS) {
+		       rdma_error("Work completion (WC) has error status: %d, %s at index %d", 
+				       wc[i].status, ibv_wc_status_str(wc[i].status), i);
+		       /* return negative value */
+		       return -(wc[i].status);
+	       }
+       }
+       /* Similar to connection management events, we need to acknowledge CQ events */
+       ibv_ack_cq_events(cq_ptr, 
+		       1 /* we received one event notification. This is not 
+		       number of WC elements */);
+       return total_wc; 
+}
+
+
+/* Code acknowledgment: rping.c from librdmacm/examples */
+int get_addr(char *dst, struct sockaddr *addr)
+{
+	struct addrinfo *res;
+	int ret = -1;
+	ret = getaddrinfo(dst, NULL, NULL, &res);
+	if (ret) {
+		rdma_error("getaddrinfo failed - invalid hostname or IP address\n");
+		return ret;
+	}
+	memcpy(addr, res->ai_addr, sizeof(struct sockaddr_in));
+	freeaddrinfo(res);
+	return ret;
+}
+
+
+
+int stick_this_thread_to_core(int core_id) {
+   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+   if (core_id < 0 || core_id >= num_cores)
+      return EINVAL;
+
+   cpu_set_t cpuset;
+   CPU_ZERO(&cpuset);
+   CPU_SET(core_id, &cpuset);
+
+   pthread_t current_thread = pthread_self();    
+   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+}
+
+
---
+++ rcuckoo_rdma/src/rdma_common.h
@@ -0,0 +1,197 @@
+/*
+ * Header file for the common RDMA routines used in the server/client example 
+ * program. 
+ *
+ * Author: Animesh Trivedi 
+ *          atrivedi@apache.org 
+ *
+ */
+
+#ifndef RDMA_COMMON_H
+#define RDMA_COMMON_H
+
+//for thread pinning
+// #define _GNU_SOURCE
+#include <sched.h> // cpu_set_t, CPU_SET
+#include <dlfcn.h>
+#include <errno.h> // EINVAL
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // sysconf
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+
+#include <netdb.h>
+#include <netinet/in.h>	
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <rdma/rdma_cma.h>
+#include <infiniband/verbs.h>
+
+
+const char * doubles = "doubles""tripples";
+
+/* Error Macro*/
+#define rdma_error(msg, ...) fprintf(stderr, "%s : %d : ERROR : "#msg, __FILE__, __LINE__, ## __VA_ARGS__);
+
+// #define ACN_RDMA_DEBUG
+#ifdef ACN_RDMA_DEBUG 
+/* Debug Macro */
+#define debug(msg, args...) do {\
+    printf("DEBUG: "msg, ## args);\
+}while(0);
+
+#else 
+
+#define debug(msg, ...) 
+
+#endif /* ACN_RDMA_DEBUG */
+
+
+/* The following numbers are specific to YAK machines and 
+ * associated MLNX CX-5 RDMA NICs */
+
+/* CPU freq on yak-00; may be different on other machines 
+ * (TIP: Turn off frequency scaling) */
+#define CPU_FREQ (2.5e9)
+
+/* MAX work requests (limited by "max_qp_wr" dev attr ) */
+#define MAX_WR (256)
+
+/* Capacity of the completion queue (CQ) (limited by "max_cqe" dev attr) */
+#define CQ_CAPACITY (2*MAX_WR)
+
+/* MAX SGE capacity (limited by "max_sge" dev attr which is 30 for CX-5s) */
+#define MAX_SGE (30)
+
+/* MAX outstnading READ/ATOMIC ops 
+ * (limited by "max_qp_init_rd_atom" and "max_qp_rd_atom" device attrs) */
+#define MAX_RD_AT_IN_FLIGHT (16)
+//#define MAX_RD_AT_IN_FLIGHT (64)
+
+/* MAX memory registrations (limited by device resources, found empiirically for our NIC) */
+#define MAX_MR (16000)
+
+/* Default port where the RDMA server is listening */
+#define DEFAULT_RDMA_PORT (20886)
+
+/* Self-imposed limit of total queue pairs */
+#define MAX_QPS 32
+
+//#define USE_DEVICE_MEMORY
+#define DEVICE_MEMORY_KB 262144
+
+/* Bit field for work request id (NOT Portable) */
+union work_req_id {
+    struct {
+		unsigned qp_num 		: 6;		// max: 64
+		unsigned window_slot 	: 10;		// max: 1024
+		unsigned unused 		: 32;		
+    } s;
+    uint64_t val;
+};
+
+
+#define MEMORY_PERMISSION (IBV_ACCESS_LOCAL_WRITE|IBV_ACCESS_REMOTE_READ|IBV_ACCESS_REMOTE_ATOMIC|IBV_ACCESS_REMOTE_WRITE)
+
+/* 
+ * We use attribute so that compiler does not step in and try to pad the structure.
+ * We use this structure to exchange information between the server and the client. 
+ *
+ * For details see: http://gcc.gnu.org/onlinedocs/gcc/Type-Attributes.html
+ */
+struct __attribute((packed)) rdma_buffer_attr {
+  uint64_t address;
+  uint32_t length;
+  union stag {
+	  /* if we send, we call it local stags */
+	  uint32_t local_stag;
+	  /* if we receive, we call it remote stag */
+	  uint32_t remote_stag;
+  }stag;
+};
+
+/* resolves a given destination name to sin_addr */
+int get_addr(char *dst, struct sockaddr *addr);
+
+/* prints RDMA buffer info structure */
+void show_rdma_buffer_attr(struct rdma_buffer_attr *attr);
+
+/* 
+ * Processes an RDMA connection management (CM) event. 
+ * @echannel: CM event channel where the event is expected. 
+ * @expected_event: Expected event type 
+ * @cm_event: where the event will be stored 
+ */
+int process_rdma_cm_event(struct rdma_event_channel *echannel, 
+		enum rdma_cm_event_type expected_event,
+		struct rdma_cm_event **cm_event);
+
+/* Allocates an RDMA buffer of size 'length' with permission permission. This 
+ * function will also register the memory and returns a memory region (MR) 
+ * identifier or NULL on error. 
+ * @pd: Protection domain where the buffer should be allocated 
+ * @length: Length of the buffer 
+ * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
+ */
+struct ibv_mr* rdma_buffer_alloc(struct ibv_pd *pd, 
+		uint32_t length, 
+		enum ibv_access_flags permission);
+
+
+struct ibv_mr* rdma_buffer_alloc_dm(struct ibv_pd *pd, uint32_t size,
+    enum ibv_access_flags permission);
+
+struct ibv_mr *rdma_buffer_register_dm(struct ibv_pd *pd, 
+		void *addr, uint32_t length, 
+		enum ibv_access_flags permission);
+
+
+/* Frees a previously allocated RDMA buffer. The buffer must be allocated by 
+ * calling rdma_buffer_alloc();
+ * @mr: RDMA memory region to free 
+ */
+void rdma_buffer_free(struct ibv_mr *mr);
+
+/* This function registers a previously allocated memory. Returns a memory region 
+ * (MR) identifier or NULL on error.
+ * @pd: protection domain where to register memory 
+ * @addr: Buffer address 
+ * @length: Length of the buffer 
+ * @permission: OR of IBV_ACCESS_* permissions as defined for the enum ibv_access_flags
+ */
+struct ibv_mr *rdma_buffer_register(struct ibv_pd *pd, 
+		void *addr, 
+		uint32_t length, 
+		enum ibv_access_flags permission);
+/* Deregisters a previously register memory 
+ * @mr: Memory region to deregister 
+ */
+void rdma_buffer_deregister(struct ibv_mr *mr);
+
+/* Processes a work completion (WC) notification. 
+ * @comp_channel: Completion channel where the notifications are expected to arrive 
+ * @wc: Array where to hold the work completion elements 
+ * @max_wc: Maximum number of expected work completion (WC) elements. wc must be 
+ *          atleast this size.
+ */
+int process_work_completion_events(struct ibv_comp_channel *comp_channel, 
+		struct ibv_wc *wc, 
+		int max_wc);
+
+/* prints some details from the cm id */
+void show_rdma_cmid(struct rdma_cm_id *id);
+
+
+int stick_this_thread_to_core(int core_id);
+
+
+#endif /* RDMA_COMMON_H */
